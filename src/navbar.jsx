ab142d48015b0f6989f7660f5e3620f062053cf8
Tomáš G <thomas2654cz@gmail.com>
2022-03-01 16:49:18 +0100

    0.4 - centered page logo (was bugging me 😖), messed up version number (1.3)

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -11,10 +11,10 @@ var tokens = [100]
 export default function Navbar() {
   return (
     <div>
-      <nav class="bg-slate-800 text-gray-200 h-14 flex items-center rounded-cool justify-between mb-1.5 ">
+      <nav class="bg-slate-800 text-gray-200 h-14 flex items-center rounded-cool flex-row mb-1.5 ">
 
         {/* Menu za pomoci .map a arraye */}
-        <div>
+        <div class = "basis-1/3">
         {navigation_array.map(item => (
                     <Link class="p-1.5 px-2 mr-2 ml-1 box-content rounded-cool bg-slate-700 
                     hover:text-orange-300 hover:bg-gray-700
@@ -27,18 +27,19 @@ export default function Navbar() {
         </div>
 
         {/* Nazev stranky */}
-        <div>
+        <div class="basis-1/3 flex justify-center">
           <Link class="p-1.5 px-2 mr-2 ml-1 box-content rounded-cool bg-slate-700 
-          hover:text-orange-300 hover:bg-gray-700
+          hover:text-orange-300 hover:bg-gray-700 text-2xl
           transition-all" 
           to="/"
           >
-            GambleGO
+            🍀 GambleGO 🎯
           </Link>
         </div>
 
         {/* Odkaz na uzivatele + tokeny */}
-          <div class="divide-x p-1.5 px-2 mr-2 ml-1 box-content rounded-cool bg-slate-700 hover:bg-gray-700 transition-all ">
+          <div class="basis-1/3 flex justify-end">
+            <div class="bg-slate-700 hover:bg-gray-700 transition-all divide-x p-1.5 px-2 mr-2 ml-1 box-content rounded-cool">
             <Link  
             class="pr-1.5 hover:text-orange-500
             transition-all"
@@ -51,6 +52,7 @@ export default function Navbar() {
             to="/inventory">
             {username}
             </Link>
+          </div>
           </div>
 
       </nav>
